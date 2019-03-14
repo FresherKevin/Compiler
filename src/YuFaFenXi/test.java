5699d20ef5ca14375db0badcefe3f9fe565e8e4a
kevin <chaokai_xu@163.com>
2019-03-14 19:26:25 +0800

    complete

---
+++ src/YuFaFenXi/test.java
@@ -0,0 +1,9 @@
+package YuFaFenXi;
+
+import java.util.Vector;
+
+public class test {
+		public static void main(String args[]){		
+			YufaCompiler compiler=new YufaCompiler("./program.txt");		
+		}
+}
---
+++ src/Yunxing/test.java
@@ -0,0 +1,11 @@
+package Yunxing;
+
+import YuFaFenXi.YufaCompiler;
+
+public class test
+{
+	public static void main(String args[])
+	{
+		Yunxing test=new Yunxing("./program.txt");		
+	}
+}
